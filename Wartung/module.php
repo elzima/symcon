3acf1af7abf11665f9d142ff4117df6a68bb19e3
elzima <marek.kretzschmar@smart-facility.com>
2025-08-20 14:23:12 +0200

    test

--- Wartung/module.php
+++ Wartung/module.php
@@ -1,23 +1,48 @@
 <?php
 
 declare(strict_types=1);
-	class Wartung extends IPSModule
+class Wartung extends IPSModule
+{
+	public function Create()
 	{
-		public function Create()
-		{
-			//Never delete this line!
-			parent::Create();
-		}
-
-		public function Destroy()
-		{
-			//Never delete this line!
-			parent::Destroy();
-		}
-
-		public function ApplyChanges()
-		{
-			//Never delete this line!
-			parent::ApplyChanges();
-		}
+		//Never delete this line!
+		parent::Create();
 	}
+
+	public function Destroy()
+	{
+		//Never delete this line!
+		parent::Destroy();
+	}
+
+	public function ApplyChanges()
+	{
+		//Never delete this line!
+		parent::ApplyChanges();
+	}
+
+	/**
+	 * Die folgenden Funktionen stehen automatisch zur Verfügung, wenn das Modul über die "Module Control" eingefügt wurden.
+	 * Die Funktionen werden, mit dem selbst eingerichteten Prefix, in PHP und JSON-RPC wie folgt zur Verfügung gestellt:
+	 *
+	 * SF_CreateStatusInfo($id);
+	 *
+	 */
+	public function CreateStatusInfo(): void
+	{
+		echo 'Instanz : ' . IPS_GetName(0) . PHP_EOL;
+		echo 'Instanz : ' . IPS_GetName(0) . PHP_EOL;
+		echo 'Runlevel: ' . IPS_GetKernelRunlevel() . PHP_EOL;
+		echo 'Datum   : ' . date('d.m.Y', IPS_GetKernelDate()) . PHP_EOL;
+		echo 'Version : ' . IPS_GetKernelVersion() . PHP_EOL;
+		echo 'Revision: ' . IPS_GetKernelRevision() . PHP_EOL;
+		echo 'Start   : ' . date('d.m.Y H:i:s', IPS_GetKernelStartTime()) . PHP_EOL;
+		echo 'Log-Dir : ' . IPS_GetLogDir() . PHP_EOL;
+		echo 'Log-Dir : ' . IPS_GetLogDir() . PHP_EOL;
+		echo 'Plattform    : ' . IPS_GetKernelPlatform() . PHP_EOL;
+		echo 'Update-Kanal : ' . IPS_GetUpdateChannel() . PHP_EOL;
+		//
+		echo 'letztes Backup    : ' . IPS_GetLogDir() . PHP_EOL;
+		echo 'Backup-Verzeichnis: ' . IPS_GetLogDir() . PHP_EOL;
+	}
+}
