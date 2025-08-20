7be0d1461a5577bdd1c86adb7a1e5e36790f5df4
elzima <marek.kretzschmar@smart-facility.com>
2025-08-20 14:15:13 +0200

    CreateBackup added

--- Backup/module.php
+++ Backup/module.php
@@ -1,23 +1,43 @@
 <?php
-
+/**
+ * 
+ */
 declare(strict_types=1);
-	class Backup extends IPSModule
+class Backup extends IPSModule
+{
+	public function Create()
 	{
-		public function Create()
-		{
-			//Never delete this line!
-			parent::Create();
-		}
-
-		public function Destroy()
-		{
-			//Never delete this line!
-			parent::Destroy();
-		}
-
-		public function ApplyChanges()
-		{
-			//Never delete this line!
-			parent::ApplyChanges();
-		}
+		//Never delete this line!
+		parent::Create();
 	}
+
+	public function Destroy()
+	{
+		//Never delete this line!
+		parent::Destroy();
+	}
+
+	public function ApplyChanges()
+	{
+		//Never delete this line!
+		parent::ApplyChanges();
+	}
+
+	/**
+	 * Die folgenden Funktionen stehen automatisch zur Verfügung, wenn das Modul über die "Module Control" eingefügt wurden.
+	 * Die Funktionen werden, mit dem selbst eingerichteten Prefix, in PHP und JSON-RPC wie folgt zur Verfügung gestellt:
+	 *
+	 * SF_CreateBackup($id);
+	 *
+	 */
+	public function CreateBackup(): void
+	{
+		echo "Funktion ist noch nicht implementiert.";
+	}
+}
+
+/**
+ * TODO:
+ * Log-Eintrag für Wartungslog aktivierbar/deaktivierbar,
+ * ggf. Instanz des Wartungslogs auswählen, besser automatisch suchen
+ */
